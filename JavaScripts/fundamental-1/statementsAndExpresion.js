54bc222a083ad422124f33444906b34df55c35ee
Hamid99Ali <mail2hamidali99@gmail.com>
2021-12-12 23:16:42 +0500

    StatementsAndExpresention

---
+++ JavaScripts/fundamental-1/statementsAndExpresion.js
@@ -0,0 +1,2 @@
+// Expersion is a piece of code that products a value.For Example three plus four is a expresion because this is can produce a value 3+4, 1551, true are all expersion.
+// A declaration is like a complete sentences and expersion are like as the word that make up the sentances. basically we write our whole program as a sequence of action and these actions are the statements 
