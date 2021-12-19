2f6c760004ba0625a19e529aef51ffcac4e9ca8b
Hamid99Ali <mail2hamidali99@gmail.com>
2021-12-19 13:08:34 +0500

    week 2

---
+++ JavaScripts/fundamental-1/code.js
@@ -0,0 +1,33 @@
+// const fullname = "Hamid Ali";
+// const age = 22;
+// const number = "0303-*******";
+
+// const fullname = prompt("Enter your Name");
+// const age = prompt("Enter your Age ");
+// const number = prompt("Enter your number");
+// console.log(fullname, age, number);
+
+
+// This Equation prodives us maximum number from the three values.
+// const x = Number(prompt("Enter the any number"));
+// const y = Number(prompt("Enter your any number"));
+// const z = Number(prompt("Enter Your any Number"));
+// const result = ((x + y) + Math.abs(x - y)) / 2;
+// const maximum = ((y + z) + Math.abs(y - z)) / 2;
+// console.log(maximum);
+
+// d = \sqrt{(x_2 - x_1)^2 + (y_2-y_1)^2}
+
+// Distance of the area
+// const x1 = Number(prompt("Enter Your any Number for x1"));
+// const x2 = Number(prompt("Enter your any Number for x2"));
+// const y1 = Number(prompt("Enter Your any Number for y1"));
+// const y2 = Number(prompt("Enter Your any Number for y2"));
+// const result = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+// console.log(result);
+
+// To find the number is even or odd.
+const number = Number(prompt("Enter the number = "));
+const result = number % 2 == 0 ? console.log(`The number is Even ${number}`) : console.log(`The Number is odd ${number}`);
+
+
